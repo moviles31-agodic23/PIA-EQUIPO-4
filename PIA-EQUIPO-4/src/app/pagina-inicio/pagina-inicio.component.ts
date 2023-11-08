98470100b3a84a86aff12b9fd0cf1f1cd3147358
César Sánchez <cesar.alexanderds@gmail.com>
2023-11-07 23:39:28 -0600

    Cambios, requerimientos inicio -Cesar

--- PIA-EQUIPO-4/src/app/pagina-inicio/pagina-inicio.component.ts
+++ PIA-EQUIPO-4/src/app/pagina-inicio/pagina-inicio.component.ts
@@ -11,4 +11,14 @@ export class PaginaInicioComponent  implements OnInit {
 
   ngOnInit() {}
 
+  perfilEjemplo = {
+    fotoPerfil: "",
+    nombrePerfil: "Cesar",
+    seguidoresPerfil: "12",
+    publicacionesPerfil: "12",
+    seguidosPerfil: "12",
+    descripcionPerfil: "Hola",
+    fotosDelPerfil: "https://scontent.fntr8-1.fna.fbcdn.net/v/t39.30808-6/355269832_6061859987275836_3978109988933382981_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=JLEPiew8O9MAX9RwLV1&_nc_ht=scontent.fntr8-1.fna&oh=00_AfDrSVrmSIizU45HzoU9PqZJzMBhDJr2DuUfet3eUcHHog&oe=654F767A"
+  }
+
 }
