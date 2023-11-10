03811120933c24c46bdbe957f417e4de02eb3915
robinson.delonmrl@uanl.edu.mx <robindd2>
2023-11-09 21:13:30 -0600

    ROBIN

--- PIA-EQUIPO-4/src/app/home/home.module.ts
+++ PIA-EQUIPO-4/src/app/home/home.module.ts
@@ -5,6 +5,9 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { LoginComponent } from '../login/login.component';
+import { PaginaInicioComponent } from '../pagina-inicio/pagina-inicio.component';
+import { PaginaPerfilComponent } from '../pagina-perfil/pagina-perfil.component';
 
 
 @NgModule({
@@ -14,6 +17,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,LoginComponent,PaginaInicioComponent,PaginaPerfilComponent]
 })
 export class HomePageModule {}
