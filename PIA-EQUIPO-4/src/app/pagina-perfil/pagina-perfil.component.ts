5ad68ec9bb0a15d457f1e85f0184e64f5d1fb05a
César Sánchez <cesar.alexanderds@gmail.com>
2023-11-23 16:07:03 -0600

    [[

--- PIA-EQUIPO-4/src/app/pagina-perfil/pagina-perfil.component.ts
+++ PIA-EQUIPO-4/src/app/pagina-perfil/pagina-perfil.component.ts
@@ -12,7 +12,7 @@ export class PaginaPerfilComponent  implements OnInit {
   ngOnInit() {}
 
   perfilEjemplo = {
-    fotoPerfil: "https://scontent.fntr8-1.fna.fbcdn.net/v/t39.30808-6/355269832_6061859987275836_3978109988933382981_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=JLEPiew8O9MAX9RwLV1&_nc_ht=scontent.fntr8-1.fna&oh=00_AfDrSVrmSIizU45HzoU9PqZJzMBhDJr2DuUfet3eUcHHog&oe=654F767A",
+    fotoPerfil: "https://media.gq.com.mx/photos/5be9d9f488903bbeb43378ef/master/w_1600%2Cc_limit/selfies_6072.jpg",
     nombrePerfil: "Cesar",
     seguidoresPerfil: "12",
     publicacionesPerfil: "12",
