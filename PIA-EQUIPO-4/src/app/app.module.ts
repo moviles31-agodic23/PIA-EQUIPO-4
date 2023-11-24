ef01d91df1a513e609f1cb8d6c9aa2e98b48b652
César Sánchez <cesar.alexanderds@gmail.com>
2023-11-23 21:30:31 -0600

    firebase agregada

--- PIA-EQUIPO-4/src/app/app.module.ts
+++ PIA-EQUIPO-4/src/app/app.module.ts
@@ -8,10 +8,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Router } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule,ReactiveFormsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule,ReactiveFormsModule, provideFirebaseApp(() => initializeApp({"projectId":"pia-equipo4-instagram","appId":"1:139104442538:web:cadeb33755405456e298d3","storageBucket":"pia-equipo4-instagram.appspot.com","apiKey":"AIzaSyAY7sORDE2A57mWSjcortp1FqE9DknLNEE","authDomain":"pia-equipo4-instagram.firebaseapp.com","messagingSenderId":"139104442538"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
